763ce5d3f2ab528b4a0523d918748a5aa3d0fcdd
Jaron Wanderley <jaronwanderley@gmail.com>
2022-12-23 17:04:36 -0300

    setup auto import components

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,6 +30,11 @@ export default defineConfig({
         'src/stores',
       ],
       vueTemplate: true,
+    }),
+    Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
     }),
   ],
 })
