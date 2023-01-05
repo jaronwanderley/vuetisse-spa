804027c1ed8543052fc878ae80b60b3fe61cec24
Jaron Wanderley <jaronwanderley@gmail.com>
2023-01-05 20:23:24 -0300

    config router to use pages plugin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import routes from '~pages'
+
+import '@unocss/reset/tailwind.css'
 import 'uno.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
