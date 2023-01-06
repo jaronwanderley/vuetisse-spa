a9cab81f7db0157a86568f3052c5d186b1eb4da4
Jaron Wanderley <jaronwanderley@gmail.com>
2023-01-05 21:40:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ import pwaConfig from './pwa.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './docs',
+  },
+
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
