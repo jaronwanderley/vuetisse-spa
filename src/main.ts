22ea22991fe6a12c030201d348a2d934d9486e04
Jaron Wanderley <jaronwanderley@gmail.com>
2023-01-09 23:39:33 -0300

    update main to use directives

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import App from './App.vue'
 import generatedRoutes from '~pages'
+
+import { sortPlugin } from '@/directives/sort'
 
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
@@ -17,5 +20,7 @@ const router = createRouter({
 
 const app = createApp(App)
 app.use(router)
+app.use(autoAnimatePlugin)
+app.use(sortPlugin)
 
 app.mount('#app')
