c0a3c17a80f9a1b3bceba04a8320c69bb4ca2733
Jaron Wanderley <jaronwanderley@gmail.com>
2023-10-03 20:36:37 -0300

    Update Home to include a service call

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,20 @@
+<script setup>
+let posts = $ref({})
+const loadPosts = () => posts = postService.getPosts()
+
+let postCount = $ref({})
+const loadPostCount = () => postCount = postService.getPostsCount()
+
+onMounted(() => {
+  loadPosts()
+  loadPostCount()
+})
+</script>
+
 <template>
   <Counter />
+  <div>{{ posts.loading ? 'Carregando...' : 'carregou!' }}</div>
+  <div v-if="!posts.loading && !postCount.loading">
+    Posts count is {{ posts.data?.length || 0}} or  {{ postCount.data }}
+  </div>
 </template>
